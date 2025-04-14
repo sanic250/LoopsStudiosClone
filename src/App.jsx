92cc93e6f9d36206f5d2667aaca13a2fb9c93716
sanic250 <115185839+sanic250@users.noreply.github.com>
2025-04-15 00:10:36 +0200

    adding sections, navbar and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,38 @@ import "./App.css";
 function App() {
   return (
     <div className="main-container">
-    <div className="navbar">
-      <div className="hero-bg"></div>
-      <div className="logo-container">
-        <img src="./src/assets/logo.svg" alt="logo"/>
+        <div className="hero-bg"></div>
+        <div className="wrapper">
+      <div className="navbar">
+          <div className="navigation">
+            <div className="logo-container">
+              <img src="./src/assets/logo.svg" alt="logo" />
+            </div>
+            <div className="nav-links">
+              <a>About</a>
+              <a>Careers</a>
+              <a>Events</a>
+              <a>Products</a>
+              <a>Support</a>
+            </div>
+          </div>
+          <div className="title">
+            <h1>IMMERSIVE EXPERIENCES THAT DELIVER</h1>
+          </div>
+        </div>
+      <section className=" section section-one">
+        <div className="section-one-photo">
+          <img src="./src/assets/desktop/image-interactive.jpg" />
+        </div>
+        <div className="section-one-right">
+            <h2>THE LEADER IN INTERACTIVE VR</h2>
+            <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+        </div>
+      </section>
+      <section className="section section-two">
+       
+      </section>
       </div>
-    </div>
     </div>
   );
 }
