ccaa75f7204360a5f4e4bf996c5e6cff348627d7
sanic250 <115185839+sanic250@users.noreply.github.com>
2025-04-14 17:46:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="main-container">
+    <div className="navbar">
+      <div className="hero-bg"></div>
+      <div className="logo-container">
+        <img src="./src/assets/logo.svg" alt="logo"/>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
